61cffc0b87a6c35a5a5f045e07dbb21fc034ea16
SPEEDER NFH <annliratanak@gmail.com>
2012-05-30 05:00:48 +0700

    Add comment for create on GIT

--- FunctionAnalysis/src/main/Main.java
+++ FunctionAnalysis/src/main/Main.java
@@ -1,3 +1,32 @@
+/*
+ * 
+ * Global setup:
+ Set up git
+  git config --global user.name "Your Name"
+  git config --global user.email annliratanak@gmail.com
+      
+Next steps:
+  mkdir FunctionAnalysis
+  cd FunctionAnalysis
+  git init
+  touch README
+  git add README
+  git commit -m 'first commit'
+  git remote add origin git@github.com:liratanak/FunctionAnalysis.git
+  git push -u origin master
+      
+Existing Git Repo?
+  cd existing_git_repo
+  git remote add origin git@github.com:liratanak/FunctionAnalysis.git
+  git push -u origin master
+      
+Importing a Subversion Repo?
+  Check out the guide for step by step instructions.
+      
+When you're done:
+  Continue
+ * 
+ * */
 package main;
 
 public class Main {
